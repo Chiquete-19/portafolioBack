a9a1b93f4391c893fef0b87dc2f6aef01508b076
Fernando Chiquete <ferchiquete2003@gmail.com>
2026-03-19 11:48:54 -0700

    SendGrid config

---
+++ src/main/java/com/chiquete/portafolio/Mail/EmailService.java
@@ -0,0 +1,30 @@
+package com.chiquete.portafolio.Mail;
+
+import org.springframework.stereotype.Service;
+
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+@Service
+public class EmailService {
+
+    public void sendEmail(String to, String subject, String content) throws Exception {
+
+        Email from = new Email("tuemail@tudominio.com");
+        Email toEmail = new Email(to);
+
+        Content body = new Content("text/html", content);
+        Mail mail = new Mail(from, subject, toEmail, body);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        sg.api(request);
+    }
+}
