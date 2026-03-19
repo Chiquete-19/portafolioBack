74c23bd655a9aef0b92eac35de668793eafe0cb7
Fernando Chiquete <l21170293@culiacan.tecnm.mx>
2026-03-19 12:34:56 -0700

    Api_key

--- src/main/java/com/chiquete/portafolio/Mail/EmailService.java
+++ src/main/java/com/chiquete/portafolio/Mail/EmailService.java
@@ -1,5 +1,6 @@
 package com.chiquete.portafolio.Mail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sendgrid.*;
@@ -10,6 +11,9 @@ import com.sendgrid.helpers.mail.objects.Email;
 @Service
 public class EmailService {
 
+    @Value("${app.api.key}")
+    private String API_KEY;
+
     public void sendEmail(String to, String subject, String content) throws Exception {
 
         Email from = new Email("tuemail@tudominio.com");
@@ -18,7 +22,7 @@ public class EmailService {
         Content body = new Content("text/html", content);
         Mail mail = new Mail(from, subject, toEmail, body);
 
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        SendGrid sg = new SendGrid(System.getenv(API_KEY));
         Request request = new Request();
 
         request.setMethod(Method.POST);
--- src/main/java/com/chiquete/portafolio/controllers/ControllerRest.java
+++ src/main/java/com/chiquete/portafolio/controllers/ControllerRest.java
@@ -2,17 +2,26 @@ package com.chiquete.portafolio.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chiquete.portafolio.Mail.EmailService;
 import com.chiquete.portafolio.dtoClasses.MailDto;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 public class ControllerRest {
 
     @PostMapping("/sendMail")
-    public void sendMail(@RequestBody MailDto mail) {
+    public ResponseEntity<?> sendMail(@RequestBody MailDto mail) throws Exception {
         
+        EmailService email=new EmailService();
+        try{
+            email.sendEmail(mail.getFrom(), "Contacto", mail.getMessage());
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
